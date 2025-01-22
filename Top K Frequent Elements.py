294b96835480f2200618d08d3a2608e04684aa53
Gnana Prakash <155003147+gnanaprakashn@users.noreply.github.com>
2025-01-22 22:35:24 +0530

    Update Top K Frequent Elements.py

--- Top K Frequent Elements.py
+++ Top K Frequent Elements.py
@@ -10,9 +10,13 @@ class Solution:
         c = { }
         for i in range(len(nums)):
             a = nums[i]
+            #my logic is i create the dictionary for every element in array 
             if a in c:
                 c[a]+=1
+            # it get count when it is already present in dictionary
             else:
                 c[a]=1
         sorted_elements = sorted(c, key=c.get, reverse=True)
+        #here i get the keys of the dic and revese 
         return sorted_elements[:k]
+        #  and do the slicing for top k frequent element
