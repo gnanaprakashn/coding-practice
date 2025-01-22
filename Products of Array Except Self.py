c6f94cbd40e28d5a610c85e5389b698be0a42916
Gnana Prakash <155003147+gnanaprakashn@users.noreply.github.com>
2025-01-22 22:31:27 +0530

    Update Products of Array Except Self.py

--- Products of Array Except Self.py
+++ Products of Array Except Self.py
@@ -6,10 +6,11 @@ from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         c = []
-       
+       # i created the list to store the product of array
         for i in range(len(nums)):
             b=1
             for j in range(len(nums)):
+                # i created the 2 loops for product it wont mutiple except self 
                 if i !=j:
                     
                     b *=nums[j] 
@@ -17,4 +18,5 @@ class Solution:
                 
               
             c.append(b)
+            #append the product of array to the list
         return c
