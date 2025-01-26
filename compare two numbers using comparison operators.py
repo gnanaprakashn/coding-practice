58ffa4f93f737fc504d535321456917a150fc536
gnanaprakashn <nagarajgnana@gmail.com>
2025-01-26 21:47:42 +0530

    today 26/1/2024 solved problems

---
+++ compare two numbers using comparison operators.py
@@ -0,0 +1,11 @@
+# compare two numbers using comparison operators
+def compare_numbers(a,b):
+    print(f"{a} < {b} is {a < b}")
+    print(f"{a} <= {b} is {a <= b}")
+    print(f"{a} > {b} is {a > b}")
+    print(f"{a} >= {b} is {a >= b}")
+    print(f"{a} == {b} is {a == b}")
+    print(f"{a} != {b} is {a != b}")
+
+
+compare_numbers(a, b)
---
+++ compare two numbers.py
@@ -0,0 +1,12 @@
+#  find the bigger number
+def finding_bigger_number(a, b):
+    if a > b:
+        print(f"The bigger number is: {a}")
+    elif a > b:
+        print(f"The bigger number is: {b}")
+    else:
+        print("Both numbers are equal.")
+
+
+
+finding_bigger_number(a, b)
